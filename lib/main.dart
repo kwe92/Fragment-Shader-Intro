97dcf40f409855c89b0d229e0e7d4530366d7ad2
Kwe <Kweayon@gmail.com>
2024-11-04 13:48:23 -0600

    refact: moved widgets into their own modules to increase code modularity

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:ui';
 
+import 'package:custom_shaders/smiley_face_with_shader.dart';
+import 'package:custom_shaders/widgets/custonGradientShader/custom_shader.dart';
 import 'package:flutter/material.dart';
 
 late FragmentProgram fragmentProgram;
@@ -17,49 +19,33 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CustomPaint(
-        painter: MyPainter(
-          Colors.purple,
-          //supply compiled shader to CustomPaint widget
-          shader: fragmentProgram.fragmentShader(),
+      home: Scaffold(
+        body: Stack(
+          children: [
+            SizedBox(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              child: CustomGradientShader(
+                Colors.purpleAccent,
+                Colors.blueAccent,
+                shader: fragmentProgram.fragmentShader(),
+              ),
+            ),
+            Positioned.fill(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                width: 100,
+                height: 100,
+                child: SmileyFaceWithShader(
+                  Colors.purpleAccent,
+                  Colors.blueAccent,
+                  shader: fragmentProgram.fragmentShader(),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
-}
-
-class MyPainter extends CustomPainter {
-  // the color that will be passed to the shader
-  final Color color;
-
-  final FragmentShader shader;
-
-  MyPainter(this.color, {required this.shader});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    // passing arguments to a fragment shader but using the FragmentShader API
-    // must look at what uniform variables the shader expects
-
-    // in our example the shader expect 6 floating point values starting with a 0-based index
-    // which represents a vector-2d (size) and a vector4d (color in RGBA)
-
-    // shader.setFloat(0, size.width);
-    // shader.setFloat(1, size.height);
-    shader.setFloat(0, (color.red.toDouble() / 255));
-    shader.setFloat(1, color.green.toDouble() / 255);
-    shader.setFloat(2, color.blue.toDouble() / 255);
-    shader.setFloat(3, color.alpha.toDouble() / 255);
-
-    // define a rectangle that fills all available space, with a suplied paint object
-    // add shader to Paint object directly using cascade
-    canvas.drawRect(
-      Rect.fromLTWH(0, 0, size.width, size.height),
-      Paint()..shader = shader,
-    );
-  }
-
-  // if properties change and do not match previous widget repaint
-  @override
-  bool shouldRepaint(MyPainter oldDelegate) => color != oldDelegate.color;
 }
